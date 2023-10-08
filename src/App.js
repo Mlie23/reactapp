3e0c2c1ba73c518359fc28fdc3884a22e4f7bdea
mlie23 <mlie23@my.whitworth.edu>
2023-10-07 21:35:28 -0700

    added navbar from todays experiment and landing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
+function App() {
+  return (
+
+    <div class="= mx-auto max-w-7xl py-2 px-4">
+      <Navbar />
+      <Landing/>
+      {/* <div class="text-7xl text-red-500">      Hello world!!!!</div> */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div class="">
+        <div>
+            This is the start of our great project
+        </div>
+        <div>
+            Copyright: Nirjal Shakya & Michael Lie.
+        </div>
+    </div>
+
+  )
+}
+
+export default Landing
